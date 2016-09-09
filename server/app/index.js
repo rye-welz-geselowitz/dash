cbe7a8339575d019fc8b066c592f70716db66143
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-09-09 19:13:34 -0400

    cleared authorization attempt decided to use passport

--- server/app/index.js
+++ server/app/index.js
@@ -1,19 +1,19 @@
 var express=require('express');
 var path = require('path');
 var session = require('express-session');
+var Company=require('../db/models/company');
+var bodyParser = require('body-parser');
 
 var app = express();
+
+//body parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //static routing
 var rootPath = path.join(__dirname, '..', '..');
 var publicPath = path.join(rootPath, 'public');
 app.use(express.static(publicPath));
-
-//logging in and signing up
-app.use(session({
-    secret: 'Optimus Prime is my real dad'
-})); //TODO: Move this to a secret file!!!
-
 
 
 app.use('/api', require('./routes'));

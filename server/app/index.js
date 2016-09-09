52a238c000514a43fb89c50ad35012d130cb0c4f
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-09-09 18:30:36 -0400

    just got started on authentication but no big decisions made yet

--- server/app/index.js
+++ server/app/index.js
@@ -1,6 +1,6 @@
 var express=require('express');
 var path = require('path');
-
+var session = require('express-session');
 
 var app = express();
 
@@ -8,6 +8,12 @@ var app = express();
 var rootPath = path.join(__dirname, '..', '..');
 var publicPath = path.join(rootPath, 'public');
 app.use(express.static(publicPath));
+
+//logging in and signing up
+app.use(session({
+    secret: 'Optimus Prime is my real dad'
+})); //TODO: Move this to a secret file!!!
+
 
 
 app.use('/api', require('./routes'));

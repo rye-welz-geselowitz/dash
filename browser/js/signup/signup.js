a143d4e5dbeaab0abcc16d07120a6ae0ccda2cd9
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-09-10 18:53:30 -0400

    scaffolding for signup and login on front end but still no routes

---
+++ browser/js/signup/signup.js
@@ -0,0 +1,39 @@
+app.config(function ($stateProvider) {
+    $stateProvider.state('signup', {
+        url: '/signup',
+        templateUrl: 'js/signup/signup.html',
+        controller: 'SignupCtrl'
+    });
+});
+
+app.controller('SignupCtrl', function($scope, AuthFactory, $state, AuthService) {
+  $scope.sendSignup = function(info) {
+    console.log('info', info);
+    AuthFactory.signup($scope.signup)
+    .then(function() {
+      return AuthService.login(info)
+    })
+    .then(function() {
+      //TODO: redirect somewhere
+      console.log('signed up!')
+    });
+  }
+});
+
+app.factory('AuthFactory', function($http, $state) {
+  var authObj = {};
+
+  authObj.currentUser = {};
+  authObj.currentUser.loggedIn = false;
+
+  authObj.signup = function(data) {
+    return $http.post('/api/company', data)
+      // .catch($log.error);
+  };
+
+  authObj.isLoggedIn = function() {
+    return authObj.currentUser.loggedIn;
+  };
+
+  return authObj;
+});

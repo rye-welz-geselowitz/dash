084e8fca02823432ce24ab7777376a12bb72d965
Sarah Welz Geselowitz <sarahgeselowitz@gmail.com>
2016-09-13 14:35:09 -0400

    sorted by name

--- browser/js/directives/employee/employee.js
+++ browser/js/directives/employee/employee.js
@@ -21,6 +21,11 @@ app.directive('employee', function (AuthService, $state, LoginFactory,$rootScope
       scope.goToDetail=function(employee){
         $state.go('employee',{employee:employee, id:employee.id});
       }
+      scope.parseDate=function(dateString){
+        var date=new Date(dateString);
+        var months={0:'Jan',1:'Feb',2:'Mar',3:'Apr',4:'May',5:'Jun',6:'Jul',7:'Aug',8:'Sep',9:'Oct',10:'Nov',11:'Dec'}
+        return date.getDate()+' '+months[date.getMonth()]+' '+date.getFullYear();
+      }
     }
 
   }
